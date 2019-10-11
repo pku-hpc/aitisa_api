da38a423c00b75e18e4a483d82c4932b0f9667a5
learlzy <491686106@qq.com>
2019-10-11 15:49:15 +0800

    Add source and test code of cast

---
+++ src/basic/cast.h
@@ -0,0 +1,15 @@
+#ifndef CAST_H
+#define CAST_H
+
+#include "src/core/tensor.h"
+
+/**
+ * @brief Applies a cast over an input signal with specified DataType.
+ * @param input The input tensor to be casted.
+ * @param type The DataType to cast to.
+ * @param output The output tensor pointer.
+ * @return Status The Status enum indicates whether the routine is OK.
+ */
+AITISA_API_PUBLIC Status aitisa_cast(const Tensor ipnut, DataType dtype,
+                                     Tensor *output);
+#endif
---
+++ test/cast_test.cpp
@@ -0,0 +1,85 @@
+#include "gtest/gtest.h"
+#include <math.h>
+#include <vector> 
+extern "C" {
+#include "src/basic/factories.h"
+#include "src/core/tensor.h"
+#include "src/basic/cast.h"
+#include "src/math/binary_op.h"
+}
+
+void cast_assign_int32(Tensor t) {
+  int64_t size = aitisa_tensor_size(t);
+  int32_t* data = (int32_t*)aitisa_tensor_data(t);
+  int32_t value = 0;
+  for (int i = 0; i < size; ++i) {
+    value = i;
+    data[i] = value;
+  }
+}
+
+void cast_assign_float(Tensor t) {
+  int64_t size = aitisa_tensor_size(t);
+  float* data = (float*)aitisa_tensor_data(t);
+  float value = 0;
+  for (int i = 0; i < size; ++i) {
+    value = i * 0.1;
+    data[i] = value;
+  }
+}
+
+namespace aitisa_api {
+namespace {
+TEST(Cast, FloatToInt32) {
+  Tensor input;
+  Tensor factor;
+  int64_t dims[2] = { 3,2 };
+  Device device = { DEVICE_CPU, 0 };
+  DataType dtype = { TYPE_FLOAT, sizeof(float) };
+  aitisa_create(dtype, device, LAYOUT_DENSE, dims, 2, &input);
+  aitisa_full(dtype, device, dims, 2, 7, &factor);
+  cast_assign_float(input);
+  aitisa_mul(input, factor, &input);
+  DataType out_dtype = { TYPE_INT32, sizeof(int32_t) };
+  
+  Tensor output;
+  Status status;
+  status = aitisa_cast(input, out_dtype, &output);
+  status = aitisa_cast(input, out_dtype, &output);
+
+  int32_t* out_data = (int32_t*)aitisa_tensor_data(output);
+  std::vector<int32_t> test_data = { 0, 0, 1, 2, 2, 3 };
+  int64_t size = aitisa_tensor_size(output);
+  for (int64_t i = 0; i < size; i++) {
+		EXPECT_TRUE(out_data[i] == test_data[i]);
+	}
+}
+
+TEST(Cast, Int32ToDouble) {
+  Tensor input;
+  Tensor factor;
+  int64_t dims[2] = { 3,2 };
+  Device device = { DEVICE_CPU, 0 };
+  DataType dtype = { TYPE_INT32, sizeof(int32_t) };
+  aitisa_create(dtype, device, LAYOUT_DENSE, dims, 2, &input);
+  aitisa_full(dtype, device, dims, 2, 2, &factor);
+  cast_assign_int32(input);
+  aitisa_mul(input, factor, &input);
+  DataType out_dtype = { TYPE_DOUBLE, sizeof(double) };
+
+  Tensor output;
+  Status status;
+  status = aitisa_cast(input, out_dtype, &output);
+  status = aitisa_cast(input, out_dtype, &output);
+
+  double* out_data = (double*)aitisa_tensor_data(output);
+  std::vector<double> test_data = { 0, 2, 4, 6, 8, 10 };
+  int64_t size = aitisa_tensor_size(output);
+  for (int64_t i = 0; i < size; i++) {
+    // Due to the problem of precision, consider the two numbers 
+    // are equal when their difference is less than 0.00001
+    EXPECT_TRUE(abs(out_data[i] - test_data[i]) < 0.00001);
+  }
+}
+}//namespace
+}//namespace aitisa_api
