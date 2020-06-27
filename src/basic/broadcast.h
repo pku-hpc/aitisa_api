73e5b1685efa23e5436777d6572026ab8f203bda
poohRui <32978179+poohRui@users.noreply.github.com>
2020-06-27 18:59:50 +0800

    Update copyright

--- src/basic/broadcast.h
+++ src/basic/broadcast.h
@@ -2,22 +2,9 @@
  * Copyright (c) 2020 ["Peking University", "Peng Cheng Laboratory"]
  *
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *    This product developed by Peking University and Peng Cheng Laboratory.
- * 4. Neither the name of the organizations (Peking University
- *    and Peng Cheng Laboratory) nor the
- *    names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
+ * The software is released under the Open-Intelligence Open Source License V2.0.
+ * you may not use this file except in compliance with the License.
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -29,8 +16,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * For more information, contact us at chao_yang@pku.edu.cn.
- **************************************************************************************/
+**************************************************************************************/
 
 #ifndef BROADCAST_H
 #define BROADCAST_H
