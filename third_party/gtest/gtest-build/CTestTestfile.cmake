b72133a883190ebab8745fa4a8ccb778507e42a2
lizy <1801213993@login01.hpc.pku.edu.cn>
2021-04-07 11:00:33 +0800

    Remove unnecessarry files

--- third_party/gtest/gtest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /gpfs/share/home/1801213993/aitisa_api/third_party/gtest/gtest-src
-# Build directory: /gpfs/share/home/1801213993/aitisa_api/third_party/gtest/gtest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- third_party/gtest/gtest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /gpfs/share/home/1801213993/aitisa_api/third_party/gtest/gtest-src/googletest
-# Build directory: /gpfs/share/home/1801213993/aitisa_api/third_party/gtest/gtest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
